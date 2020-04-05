a132fa8fd1f213890cd8e6c0903a7fa31776fccd
Sayan <chowdhury.sayan4@gmail.com>
2020-04-05 11:32:47 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Home from './containers/Home/Home';
+
+class App extends Component{
+  render(){
+
+    return(
+<div>
+<Home>
+
+</Home>
+</div>
+    );
+  }
+}
+
+export default App;
